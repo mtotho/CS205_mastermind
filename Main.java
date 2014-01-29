96ac8ab9726e414a57a361bc563f930935e8a941
cwilli13 <ryanwilliams92@gmail.com>
2014-01-29 13:15:51 -0500

    Undid accidental changes

--- Main.java
+++ Main.java
@@ -1,43 +1,9 @@
-import java.util.Scanner;
-
 public class Main{
 
 	public static void main(String[] args) {
-		/* initialize variables for player */
-		int userWins = 0;
-		int userLosses = 0;
-		Scanner userInput = new Scanner(System.in);
-
-		/* Get player's name */
-		System.out.println("Enter Your Name:");
 		
-		String userName = userInput.nextLine();
-
-		System.out.println("Choose a difficulty:\n1 = Easy\n2 = Hard");
-
-		String diffString = userInput.nextLine();
-		int userDifficulty = Integer.parseInt(diffString);
-		Player p1 = new Player(userName, userWins, userLosses, userDifficulty);
-
-		/* test player functions */
-		/*System.out.println("Test functions:");
-
-		String testName = "Walter White";
-		p1.setName(testName);
-		System.out.println("\n1) Set Name & Get Name:");
-		System.out.println("Name = " + p1.getName());
-
-		p1.addWin();
-		System.out.println("\n2) Add Win:");
-		System.out.println("Win = " + p1.getWins());
-
-		p1.addLoss();
-		System.out.println("\n3) Add Loss:");
-		System.out.println("Loss = " + p1.getLosses());*/
-
-
-
-
+		
+		Player p1 = new Player();
 
 		//Logic to switch easy hard
 
--- Player.java
+++ Player.java
@@ -1,83 +1,8 @@
-/* C. Ryan Williams 1/27/14
-	
-Set Player class. Will include;
-	- string name
-	- int wins
-	- int losses
-	- int difficulty
-	 */
-
-import java.util.Scanner;
-/*import java.util.*; */
-
 public class Player{
 
-	private String name;
-	private int wins;
-	private int losses;
-	private int difficulty;
+	public Player(){
+		System.out.println("player created");
 
-	Scanner userInput = new Scanner(System.in);
-
-	public Player(String userName, int userWins, int userLosses, int userDifficulty){
-		System.out.println("Creating Player...");
-		System.out.println("Player created.");
-
-
-		name = userName;
-		wins = userWins;
-		losses = userLosses;
-		difficulty = userDifficulty;
-
-
-		System.out.println("\n\n\nWelcome to Mastermind " + name + ".");
-
-		String difficultyString;
-
-		switch (difficulty){
-			case 1: difficultyString = "Easy";
-										break;
-			case 2: difficultyString = "Hard";
-										break;
-			default: difficultyString = "Error";
-										break;
-		}
-		System.out.println("You are playing Mastermind on " + difficultyString + ".");
-		System.out.println("\nYour record:");
-		System.out.println("Wins - " + wins);
-		System.out.println("Losses - " + losses + "\n");
-
+		
 	}
-
-	public void setName(String newName){
-		name = newName;
-	}
-
-	public String getName(){
-		return name;
-	}
-
-	public void setDifficulty(int newDifficulty){
-		difficulty = newDifficulty;
-	}
-
-	public int getDifficulty(){
-		return difficulty;
-	}
-
-	public void addWin(){
-		wins++;
-	}
-
-	public int getWins(){
-		return wins;
-	}
-
-	public void addLoss(){
-		losses++;
-	}
-	public int getLosses(){
-		return losses;
-	}
-
 }
